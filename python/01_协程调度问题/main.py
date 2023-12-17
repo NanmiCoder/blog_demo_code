eb9705af3a9098b06ea558ce8a822c89461309db
Relakkes <relakkes@gmail.com>
2023-12-17 20:14:40 +0800

    feat: 完成幂等性的代码及文档

--- python/01_协程调度问题/main.py
+++ python/01_协程调度问题/main.py
@@ -10,6 +10,8 @@ import uvicorn
 from db import init_pool
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+import redis
+import time
 
 app = FastAPI(debug=True)
 
@@ -34,7 +36,7 @@ async def init_db():
 @app.post("/register")
 async def register_user(user_info: models.UserRegistrationRequest):
     """
-    注册用户 - 普通拍版本
+    注册用户 - 普通版本
     :param user_info:
     :return:
     """
@@ -44,17 +46,46 @@ async def register_user(user_info: models.UserRegistrationRequest):
     return JSONResponse({"message": "注册失败，Openid已存在", "code": -1})
 
 
+import anyio
+
+# 用于存储每个 openid 对应的锁
+openid_locks = {}
+
+
 @app.post("/register_asyncio_lock")
 async def register_user(user_info: models.UserRegistrationRequest):
     """
-    注册用户 - 并发控制
+    注册用户 - 互斥锁Lock()
     :param user_info:
     :return:
     """
-    if not await models.check_user_exist_by_openid_use_asyncio_lock(user_info.openid):
-        await models.add_new_user(user_info.model_dump())
-        return JSONResponse({"message": "注册成功", "code": 0})
-    return JSONResponse({"message": "注册失败，Openid已存在", "code": -1})
+    lock = openid_locks.setdefault(user_info.openid, anyio.Lock())
+    async with lock:
+        if not await models.check_user_exist_by_openid(user_info.openid):
+            await models.add_new_user(user_info.model_dump())
+            return JSONResponse({"message": "注册成功", "code": 0})
+        return JSONResponse({"message": "注册失败，Openid已存在", "code": -1})
+
+
+def acquire_lock(_redis_conn: redis.Redis, lock_name: str, acquire_timeout=10):
+    identifier = str(time.time())  # 生成一个唯一的标识符
+    lock_key = f"lock:{lock_name}"
+
+    # 尝试获取锁，设置成功则返回标识符，否则等待一段时间再重试
+
+    if _redis_conn.setnx(lock_key, identifier):
+        _redis_conn.expire(lock_key, acquire_timeout)
+        return identifier
+
+
+def release_lock(_redis_conn, lock_name, identifier):
+    lock_key = f"lock:{lock_name}"
+    current_value = _redis_conn.get(lock_key)
+
+    if current_value and current_value.decode() == identifier:
+        _redis_conn.delete(lock_key)
+    else:
+        raise ValueError("Invalid lock identifier")
 
 
 @app.post("/register_redis_lock")
@@ -64,10 +95,27 @@ async def register_user(user_info: models.UserRegistrationRequest):
     :param user_info:
     :return:
     """
-    if not await models.check_user_exist_by_openid_use_asyncio_lock(user_info.openid):
-        await models.add_new_user(user_info.model_dump())
-        return JSONResponse({"message": "注册成功", "code": 0})
-    return JSONResponse({"message": "注册失败，Openid已存在", "code": -1})
+    redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0,
+                                   password=os.getenv("REDIS_DB_PWD", "you redis db pwd"))
+
+    lock_name = f"register_lock:{user_info.openid}"
+
+    # 尝试获取分布式锁
+    identifier = acquire_lock(redis_conn, lock_name)
+    if not identifier:
+        return JSONResponse({"message": "注册失败，无法获取锁", "code": -1})
+
+    try:
+        # 检查用户是否已存在
+        if not await models.check_user_exist_by_openid(user_info.openid):
+            # 用户不存在，进行注册
+            await models.add_new_user(user_info.model_dump())
+            return JSONResponse({"message": "注册成功", "code": 0})
+        else:
+            return JSONResponse({"message": "注册失败，Openid已存在", "code": -1})
+    finally:
+        # 释放分布式锁
+        release_lock(redis_conn, lock_name, identifier)
 
 
 if __name__ == '__main__':
--- python/01_协程调度问题/models.py
+++ python/01_协程调度问题/models.py
@@ -36,32 +36,3 @@ async def add_new_user(user_item: Dict) -> int:
     """
     return await db.user_db.item_to_table(table_name="users", item=user_item)
 
-
-# 在模块级别定义一个锁
-registration_lock = asyncio.Lock()
-
-async def check_user_exist_by_openid_use_asyncio_lock(openid: str) -> bool:
-    """
-    判断 openid 相关的用户是否已存在 - 使用asyncio.Lock()来防止查询并发问题
-    :param openid:
-    :return:
-    """
-    async with registration_lock:
-        sql: str = "select * from users where openid = %s"
-        data = await db.user_db.get_first(sql, openid)
-        if data:
-            return True
-        return False
-
-
-async def check_user_exist_by_openid_use_redis_lock(openid: str) -> bool:
-    """
-    判断 openid 相关的用户是否已存在 - 使用asyncio.Lock()来防止查询并发问题
-    :param openid:
-    :return:
-    """
-    sql: str = "select * from users where openid = %s"
-    data = await db.user_db.get_first(sql, openid)
-    if data:
-        return True
-    return False
